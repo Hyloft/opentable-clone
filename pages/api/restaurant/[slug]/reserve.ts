08b0c835096be8a81408b8155d079310fadb405a
Hyper <arslanburak28@hotmail.com>
2023-08-27 12:59:07 +0300

    reservation endpoint - fetch restaurant by slug and time filter

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,10 +1,37 @@
+import { prisma } from "@/app/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { day, time, partySize } = req.query;
+  const { day, time, partySize, slug } = req.query as {
+    day: string;
+    time: string;
+    partySize: string;
+    slug: string;
+  };
+
+  const invalidDataError = (error?: string) => {
+    return res.status(400).json({
+      errorMessage: error || "Invalid data provided",
+    });
+  };
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+    select: {
+      open_time: true,
+      close_time: true,
+    },
+  });
+
+  if (!restaurant) return invalidDataError("No restaurant found");
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+  ) return invalidDataError("Restaurant is not open at that time");
 
   return res.json({ day, time, partySize });
 }
